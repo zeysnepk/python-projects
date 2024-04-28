abc657fa2496ef6bf6d76b5ebc0cc268545e3b36
zeysnepk <zeyneplay7@gmail.com>
2024-04-29 01:09:35 +0300

    tuple

---
+++ tuple.py
@@ -0,0 +1,34 @@
+
+#tuple sadece okunur degistirilemez perfonmas icin ideal
+tupleListe = (2,4,6,"hello",(1,2,3))
+liste = [2,4,6,"hello",[1,2,3]]
+
+liste[0] = 6  #bunu tuple ile yazamayiz hata verir
+
+print(tupleListe)
+print(liste)
+
+print(type(tupleListe))
+print(type(liste))
+
+print(len(tupleListe))
+print(len(liste))
+
+print(tupleListe[-2])
+print(liste[-2])
+
+#2 dahil degil
+print(tupleListe[1:2]) #burda virgulle ayirir 
+print(liste[1:2])
+
+#tek elemanda tuple asagidaki gibi farkli calisir
+tupleOne = ("Zeynep")
+print(tupleOne)
+print(type(tupleOne)) #burda tipi str olarak gosterilir bunu duzeltmek icin virgul eklenmeli
+
+tupleTwo = ("Zey",)
+print(tupleTwo)
+print(type(tupleTwo))
+
+
+
