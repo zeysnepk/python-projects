9bd1f72a60247c64bccf087b223acf658699d4fe
zeysnepk <zeyneplay7@gmail.com>
2024-05-05 23:27:45 +0300

    module

---
+++ modules.py
@@ -0,0 +1,31 @@
+#modules with different functions
+
+import math
+
+print(math.factorial(5))
+print(math.floor(5.6))
+print(math.ceil(5.6))
+
+#to rename the module
+
+import math as matematik
+
+print(matematik.factorial(6))
+
+
+#get functions from modules
+
+from math import *
+
+print(floor(6.4))
+
+
+#get desired functions from modules
+
+from math import floor,cell
+
+
+
+
+
+
